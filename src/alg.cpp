4d50aad75bdd339877a474a2b96081e23ace6fab
TumanovaAnna <118038015+TumanovaAnna@users.noreply.github.com>
2023-02-25 10:31:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,43 +1,39 @@
 #include <cstdint>
 #include "alg.h"
 bool checkPrime(uint64_t value) {
-  if (value <= 1)
-    return false;
-  for (int i = 2; i <= value/2; i++) {
-    if (value % i == 0)
-      return false;
-  }
-  return true;
+    uint64_t k = 0;
+    for (uint64_t i = 2; i <= (value / 2); i++) {
+        if (value % i == 0) 
+        return false;
+    }
+    return true;
 }
 uint64_t nPrime(uint64_t n) {
-  uint64_t num = 0;
-  uint64_t num1 = 0;
-  while (n > 0) {
-    if (checkPrime(num) == true) {
-      num1 = num;
-      --n;
+    uint64_t a = 1;
+    uint64_t num = 0;
+    while (num != n) {
+        a++;
+        if (checkPrime(otvet) == true) num++;
     }
-    ++num;
-  }
-  return num1;
+    return a;
 }
 uint64_t nextPrime(uint64_t value) {
-  while (true) {
-    value++;
-    if (checkPrime(value)) {
-      return value;
+    uint64_t b = 0;
+    while (b == 0) {
+        value++;
+        if (checkPrime(value) == true) {
+            b = 1;
+            return value;
+        }
     }
-  }
+    return value;
 }
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
-  uint64_t newhb;
-  newhb = hbound - 1;
-  while (newhb > 0) {
-    if (checkPrime(newhb) == true) {
-      sum += newhb;
+    uint64_t num1 = 2;
+    uint64_t c = 0;
+    while (num1 < hbound) {
+        if (checkPrime(num1) == true) c += num1;
+        num1++;
     }
-    --newhb;
-  }
-  return sum;
+    return c;
 }
