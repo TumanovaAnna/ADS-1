c72a18d87bcebc9c5729fc4d0e10f8d39826a143
TumanovaAnna <118038015+TumanovaAnna@users.noreply.github.com>
2023-02-26 09:44:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,41 +1,44 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-bool checkPrime(uint64_t value){
-  if (value <= 1)
-    return false;
-  for (int i=2; i*i<= value; i++){
-    if (value%i==0) return false;}
+bool checkPrime(uint64_t value) {
+  for (int i = 2; i*i <= value; i++) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
   return true;
 }
 uint64_t nPrime(uint64_t n) {
-  uint64_t num=0;
-  uint64_t nomer=0;
-  while (n > 0) {
-      if (checkPrime(chislo)==1) {
-      nomer=num;
-      --n;
+  int res = 0, num = 0;
+  for (int i = 2; num < n; i++) {
+    if (checkPrime(i)) {
+      res = i;
+      num++;
     }
-    ++num;
+    return res;
   }
-  return nomer;
 }
 uint64_t nextPrime(uint64_t value) {
-  while (true) {
-    value++;
-    if (checkPrime(value)) {
-      return value;
-    }
-  }
+  int i = value + 1;
+  while (!checkPrime(i))
+    i++;
+  return i;
 }
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t summ =0;
-  hbound = hbound-1;
-  while (hbound>0) {
-    if (checkPrime(hbound)) {
-      summ = summ+hbound;
+  int sum = 0;
+  for (int i = 2; i < hbound; i++)
+    if (checkPrime(i))
+      sum = sum + i;
+  return sum;  
+}
+int64_t nPrime(uint64_t n) {
+  uint64_t res = 0, num = 0;
+  for (uint64_t i = 2; num < n; i++) {
+    if (checkPrime(i)) {
+      res = i;
+      num++;
     }
-    --hbound;
   }
-  return summ;
+  return res;
 }
